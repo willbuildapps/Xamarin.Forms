b8d27e4718f3afcfb99a175e7b511ac4ab5fba2e
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-08-11 09:26:17 +0200

    Fixed Shapes drawing inconsistencies on Android

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue11498.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+
+#if UITEST
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using NUnit.Framework;
+using Xamarin.Forms.Core.UITests;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.Brush)]
+#endif
+#if APP
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 11498, "[Bug] [Shapes] Drawing inconsistencies", PlatformAffected.Android)]
+	public partial class Issue11498 : TestContentPage
+	{
+		public Issue11498()
+		{
+#if APP
+			Title = "Issue 11498";
+			Device.SetFlags(new List<string> { ExperimentalFlags.ShapesExperimental });
+			InitializeComponent();
+#endif
+		}
+
+		protected override void Init()
+		{
+
+		}
+	}
+}
